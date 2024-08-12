5926e54215fab2bb28dec8889fd855d40ae82599
hzrd149 <github@hzrd149.com>
2024-08-12 10:41:56 -0500

    fix dev

--- src/bin/proxy.ts
+++ src/bin/proxy.ts
@@ -4,7 +4,7 @@ import createDebug from "debug";
 import { spawn } from "child_process";
 import { once } from "events";
 // @ts-expect-error no types for "basic-auth-parser"
-import basicAuthParser = require("basic-auth-parser");
+import basicAuthParser from "basic-auth-parser";
 import { createProxy } from "../proxy.js";
 //import pkg from '../pkg';
 
